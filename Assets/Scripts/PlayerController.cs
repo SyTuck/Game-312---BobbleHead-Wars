4edc951b42aed4ff57498fe3ebfd654331a31cb8
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-09-23 16:01:28 -0400

    Camera Shaking and hit sound and hit hold off

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,9 +9,14 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     public Animator bodyAnimator;
+    public float[] hitForce;
+    public float timeBetweenHits = 2.5f;
 
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
+    private bool isHit = false;
+    private float timeSinceHit = 0.0f;
+    private int hitNumber = -1;
 
     void Start()
     {
@@ -25,12 +30,15 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
 
-/*
-        Vector3 pos = this.transform.position;                              //modify the player's current position
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;  //by the value/directions of the input manager's key presses
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;    //(horizontal and vertical are tags for the arrow and WASD keys)
-        this.transform.position = pos;                                      //(deltaTime is to make a frame rate independent scale for the speed per update loop)
-   */
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -60,6 +68,31 @@ public class PlayerController : MonoBehaviour
             Quaternion rotation = Quaternion.LookRotation(targetPosition - this.transform.position);
 
             this.transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        {
+            if (!isHit)
+            {
+                hitNumber += 1;
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                if (hitNumber <hitForce.Length)
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    //death todo
+                }
+                isHit = true;
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
         }
     }
 }
