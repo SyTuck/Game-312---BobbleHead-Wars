ed2eb8e47c96ac2bd10a77e231d460fb8aae25e4
JORDAN TUCK [Student] <jordan.tuck@sl.on.ca>
2019-09-24 12:17:00 -0400

    Wall animation working, Script order and Alien decapataion done. player control broken

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -10,6 +10,9 @@ public class Alien : MonoBehaviour
     public Transform target;
     public float navigationUpdate;
     public UnityEvent OnDestroy;
+    public Rigidbody head;
+    public bool isAlive = true;
+
 
     private NavMeshAgent agent;
     private float navigationTime = 0.0f;
@@ -25,27 +28,42 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0.0f;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0.0f;
+                }
             }
-        }        
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Die();
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
-        Destroy(gameObject);
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        Destroy(gameObject);
     }
 }
